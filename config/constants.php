6c2ee06c946d40c3a0e04bc60eb6f702f07bb761
Saurabh Ghosh <saurabh.ghosh@nichi.com>
2022-10-11 18:27:14 +0530

    Payload for shopify api change

--- config/constants.php
+++ config/constants.php
@@ -6,7 +6,7 @@ return [
         'url' => 'https://pilate-wine.myshopify.com/api/2022-10/graphql.json',
         'token' => ['X-Shopify-Storefront-Access-Token' => 'f00502bffe1c0aeff048ab22cb87279a'],
         'payload' => [
-            'collectionList' => '{ collections(first: 30) { nodes { handle, title, id, products(first: 250) { nodes { handle, id, title } } image { url } } } }'
+            'collectionList' => '{ collections(first: 250) { nodes { handle, title, id, products(first: 250) { nodes { handle, id, title } } image { url } } } }'
         ]
     ]
 ];
